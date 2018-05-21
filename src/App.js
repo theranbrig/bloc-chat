62de34ff2439c46dd06d58d4e9418eab82267808
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-05-21 23:09:18 +0900

    refactored with material-ui

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import * as firebase from 'firebase'
 import RoomList from './components/RoomList'
 import MessageList from './components/MessageList'
 import User from "./components/User";
-import { Grid, Col, Row, Jumbotron } from "react-bootstrap";
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+// First we need the theme provider and the theme creator
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+
+// For this example I'll also be using the amber and blue color profiles
+import pink from '@material-ui/core/colors/pink';
+import blue from '@material-ui/core/colors/cyan';
+
+// Now let us create our theme
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'light',
+        primary: blue,
+        secondary: pink
+    }
+});
 
 // Firebase initialization
 
@@ -41,41 +61,39 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        
-        <Grid fluid>
-          <Row>
-            <Jumbotron bsClass='primay'>
-              <h1><i className="far fa-comment"></i> Bloc Chat</h1>
-              
-            </Jumbotron>
-          </Row>
-          <Row className='mainContentArea'>
+    <MuiThemeProvider theme={theme}>
+      <Grid className="App">
+        <Grid justify='center' container spacing={8}>
+          <AppBar position='static' color='primary'>
+            <Toolbar>
+              <Typography variant='title' color="inherit" aria-label="Menu"><i className="far fa-comment"></i> Bloc Chat</Typography>
+            </Toolbar>
+          </AppBar>
           {/* Room List Area */}
-            <Col xs={12} lg={3}>
-              <User 
-                firebase={firebase} 
-                setUser={ this.setUser.bind(this) } 
-                currentUser={ this.state.user === null ? 'Guest' : this.state.user.displayName } 
-                user={ this.state.user }
-              />
-              <RoomList 
+          <Grid item xs={12} md={3} className='leftArea'>
+            <User 
               firebase={firebase} 
-              activeRoom={ this.state.activeRoom } 
-              selectRoom={ this.selectRoom.bind(this) }
+              setUser={ this.setUser.bind(this) } 
+              currentUser={ this.state.user === null ? 'Guest' : this.state.user.displayName } 
+              user={ this.state.user }
             />
-            </Col>
-          {/* Chat Area */}
-            <Col xs={12} lg={9}>
-              <MessageList 
-                firebase={firebase} 
-                activeRoom={ this.state.activeRoom }
-                currentUser={ this.state.user === null ? 'Guest' : this.state.user.displayName }
-              />
-            </Col>
-          </Row>
+            <RoomList 
+            firebase={firebase} 
+            activeRoom={ this.state.activeRoom } 
+            selectRoom={ this.selectRoom.bind(this) }
+            />
+          </Grid>
+        {/* Chat Area */}
+          <Grid item xs={12} md={8}>
+            <MessageList 
+              firebase={firebase} 
+              activeRoom={ this.state.activeRoom }
+              currentUser={ this.state.user === null ? 'Guest' : this.state.user.displayName }
+            />
+          </Grid>
         </Grid>
-      </div>
+      </Grid>
+    </MuiThemeProvider>
     );
   }
 }
