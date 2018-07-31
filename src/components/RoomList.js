3fbd4795af3024106d7f170c7f835d5f2c27a6f6
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-01 00:24:19 +0900

    fixed code styling and linting

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,184 +1,202 @@
 import React, { Component } from 'react';
-import { MenuList, MenuItem, ListItemText, Typography, Button, TextField, Paper } from "@material-ui/core";
+import {
+	MenuList,
+	MenuItem,
+	ListItemText,
+	Typography,
+	Button,
+	TextField,
+	Paper
+} from '@material-ui/core';
 
 class RoomList extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      rooms: [],
-      newRoom: '',
-      editMode: false,
-      editRoomName: ''
-    };
-    
-    // Room reference and bind handlers
-    this.roomsRef = this.props.firebase.database().ref('rooms');
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.showEditButton = this.showEditButton.bind(this);
-    this.hideEditButton = this.hideEditButton.bind(this);
-    this.handleEditChange = this.handleEditChange.bind(this);
-    this.handleEditSubmit = this.handleEditSubmit.bind(this);
-  }
-  
-  // Mount RoomList from firebase
-  componentDidMount() {
-    this.roomsRef.on('value', snapshot => {
-      const roomChanges = [];
-      snapshot.forEach((room) => {
-        roomChanges.push({
-          key: room.key,
-          name: room.val().name
-        })
-      })
-      this.setState({
-        rooms: roomChanges
-      })
-    })
-  }
+	constructor(props) {
+		super(props);
 
-  // Handle input change function
+		this.state = {
+			rooms: [],
+			newRoom: '',
+			editMode: false,
+			editRoomName: ''
+		};
 
-  handleEditChange(e) {
-    this.setState({
-      editRoomName: e.target.value
-    })
-  }
+		// Room reference and bind handlers
+		this.roomsRef = this.props.firebase.database().ref('rooms');
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.showEditButton = this.showEditButton.bind(this);
+		this.hideEditButton = this.hideEditButton.bind(this);
+		this.handleEditChange = this.handleEditChange.bind(this);
+		this.handleEditSubmit = this.handleEditSubmit.bind(this);
+	}
 
-  // Handle submit for new room
+	// Mount RoomList from firebase
+	componentDidMount() {
+		this.roomsRef.on('value', snapshot => {
+			const roomChanges = [];
+			snapshot.forEach(room => {
+				roomChanges.push({
+					key: room.key,
+					name: room.val().name
+				});
+			});
+			this.setState({
+				rooms: roomChanges
+			});
+		});
+	}
 
-  handleEditSubmit(e) {
-    e.preventDefault();
-    if (this.state.editRoomName !== '') {
-      console.log('You edited the room');
-      this.roomsRef.child(this.props.activeRoom.key).set({
-        name: this.state.editRoomName
-      })
-      this.setState({
-        editRoomName: '',
-        editMode: false
-      })
-    }
-  }
+	// Handle input change function
 
-  // Handle input change function
+	handleEditChange(e) {
+		this.setState({
+			editRoomName: e.target.value
+		});
+	}
 
-  handleChange(e) {
-    this.setState({
-      newRoom: e.target.value
-    })
-  }
+	// Handle submit for new room
 
-  // Handle submit for new room
+	handleEditSubmit(e) {
+		e.preventDefault();
+		if (this.state.editRoomName !== '') {
+			console.log('You edited the room');
+			this.roomsRef.child(this.props.activeRoom.key).set({
+				name: this.state.editRoomName
+			});
+			this.setState({
+				editRoomName: '',
+				editMode: false
+			});
+		}
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const room = {
-      name: this.state.newRoom
-    }
-    if( room.name !== '') {
-      this.roomsRef.push(room);
-      this.setState({
-        newRoom: ''
-      })
-    }
-  }
+	// Handle input change function
 
-  handleDelete(itemId) {
-    if (window.confirm('Are you sure you want to delete this item?')) {
-      this.roomsRef.child(itemId).remove(function(error) {
-        if (error) {
-          console.log(error);
-        }
-      });
-    }
-  }
+	handleChange(e) {
+		this.setState({
+			newRoom: e.target.value
+		});
+	}
 
-  showEditButton() {
-    this.setState({editMode: true});
-  }
+	// Handle submit for new room
 
-  hideEditButton() {
-    this.setState({editMode: false})
-  }
-  
-  // Create room list
-  render() {
+	handleSubmit(e) {
+		e.preventDefault();
+		const room = {
+			name: this.state.newRoom
+		};
+		if (room.name !== '') {
+			this.roomsRef.push(room);
+			this.setState({
+				newRoom: ''
+			});
+		}
+	}
 
-    // Edit form buttons and input change
+	handleDelete(itemId) {
+		if (window.confirm('Are you sure you want to delete this item?')) {
+			this.roomsRef.child(itemId).remove(function(error) {
+				if (error) {
+					console.log(error);
+				}
+			});
+		}
+	}
 
-    const editMode = this.state.editMode;
-    const editButton = editMode ? (
-      <span onClick={this.hideEditButton}><i className="fas fa-ban"></i></span>
-    ) : (
-      <span onClick={ this.showEditButton }><i className='fas fa-edit'></i></span>
-    )
-    const editForm = editMode ? (
-      <div>
-        <form className='submitRoomArea' onSubmit={this.handleEditSubmit}>
-          <TextField type='text' name='newRoom' 
-            onChange={ this.handleEditChange } 
-            value={ this.state.editRoomName }
-            className='classListField'
-            label='Edit Room Name'
-          />
-          <Button 
-            type='submit' 
-            variant="raised" 
-            color="secondary" 
-            className='classListButton editButton'
-            >
-            <i className="fas fa-pen-square"></i> 
-            Edit Name
-          </Button>
-        </form>
-      </div>
-    ) : (
-      <div>
-        <form className='submitRoomArea' onSubmit={ this.handleSubmit }>
-          <TextField type='text' name='newRoom'
-            onChange={ this.handleChange }
-            value={ this.state.newRoom }
-            className='classListField'
-            label='New Room Name'
-          />
-          <Button 
-            type='submit' 
-            variant="raised" 
-            color="primary" 
-            className='classListButton'>
-            <i className="fas fa-plus-circle"></i> 
-            Add Room
-          </Button>
-        </form>
-      </div>
-    )
+	showEditButton() {
+		this.setState({ editMode: true });
+	}
 
-    return (
-        <section className='chatRoomArea'>
-          <Paper>
-            <Typography variant="headline" component="h3" color='primary'>Chat Rooms</Typography>
-          </Paper>
-            <MenuList id='roomList'>
-              {
-                this.state.rooms.map( (room, index) => 
-                  <MenuItem 
-                    key={index} 
-                    onClick={ (e) => this.props.selectRoom(room) }
-                    className='roomItem'
-                  >
-                    <span onClick={ () => this.handleDelete( room.key ) }><i className='fas fa-times-circle'> </i></span>
-                    { editButton }
-                    <ListItemText>{ room.name }</ListItemText>
-                  </MenuItem>
-                )
-              }
-            </MenuList>
-            {editForm}
-        </section>
-    )
-  }
+	hideEditButton() {
+		this.setState({ editMode: false });
+	}
+
+	// Create room list
+	render() {
+		// Edit form buttons and input change
+
+		const editMode = this.state.editMode;
+		const editButton = editMode ? (
+			<span onClick={this.hideEditButton}>
+				<i className="fas fa-ban" />
+			</span>
+		) : (
+			<span onClick={this.showEditButton}>
+				<i className="fas fa-edit" />
+			</span>
+		);
+		const editForm = editMode ? (
+			<div>
+				<form className="submitRoomArea" onSubmit={this.handleEditSubmit}>
+					<TextField
+						type="text"
+						name="newRoom"
+						onChange={this.handleEditChange}
+						value={this.state.editRoomName}
+						className="classListField"
+						label="Edit Room Name"
+					/>
+					<Button
+						type="submit"
+						variant="raised"
+						color="secondary"
+						className="classListButton editButton"
+					>
+						<i className="fas fa-pen-square" />
+						Edit Name
+					</Button>
+				</form>
+			</div>
+		) : (
+			<div>
+				<form className="submitRoomArea" onSubmit={this.handleSubmit}>
+					<TextField
+						type="text"
+						name="newRoom"
+						onChange={this.handleChange}
+						value={this.state.newRoom}
+						className="classListField"
+						label="New Room Name"
+					/>
+					<Button
+						type="submit"
+						variant="raised"
+						color="primary"
+						className="classListButton"
+					>
+						<i className="fas fa-plus-circle" />
+						Add Room
+					</Button>
+				</form>
+			</div>
+		);
+
+		return (
+			<section className="chatRoomArea">
+				<Paper>
+					<Typography variant="headline" component="h3" color="primary">
+						Chat Rooms
+					</Typography>
+				</Paper>
+				<MenuList id="roomList">
+					{this.state.rooms.map((room, index) => (
+						<MenuItem
+							key={index}
+							onClick={e => this.props.selectRoom(room)}
+							className="roomItem"
+						>
+							<span onClick={() => this.handleDelete(room.key)}>
+								<i className="fas fa-times-circle"> </i>
+							</span>
+							{editButton}
+							<ListItemText>{room.name}</ListItemText>
+						</MenuItem>
+					))}
+				</MenuList>
+				{editForm}
+			</section>
+		);
+	}
 }
 
 export default RoomList;
--- src/components/User.js
+++ src/components/User.js
@@ -2,58 +2,64 @@ import React, { Component } from 'react';
 import { Typography, Button } from '@material-ui/core';
 
 class User extends Component {
-  
-  signIn () {
-    console.log('clickSignIn')
-    const provider = new this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopup( provider ).then((result) => {
-        const user = result.user;
-        this.setState({ user });
-    });
-  }
+	signIn() {
+		console.log('clickSignIn');
+		const provider = new this.props.firebase.auth.GoogleAuthProvider();
+		this.props.firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then(result => {
+				const user = result.user;
+				this.setState({ user });
+			});
+	}
 
-  signOut () {
-    console.log('clickSignOut');
-    this.props.firebase.auth().signOut();
-    this.setState({ user: null })
-  }
+	signOut() {
+		console.log('clickSignOut');
+		this.props.firebase.auth().signOut();
+		this.setState({ user: null });
+	}
 
-  componentDidMount () {
-    this.props.firebase.auth().onAuthStateChanged(user => {
-      this.props.setUser(user);
-    });
-  }
+	componentDidMount() {
+		this.props.firebase.auth().onAuthStateChanged(user => {
+			this.props.setUser(user);
+		});
+	}
 
-  render() {
-    return (
-      <div className='userInfo'>
-        <div className='userName'>
-          <Typography component='h1' variant='headline'>Welcome { this.props.currentUser }</Typography>
-        </div>
-        <div>
-          {this.props.user === null ?
-          <Button 
-            variant='raised' 
-            color='primary' 
-            onClick={ this.signIn.bind(this) }
-            className='classListButton'>
-            <i className="fas fa-user"></i> 
-            Sign In
-          </Button>
-          :
-          <Button 
-            variant='raised' 
-            color='secondary' 
-            onClick={ this.signOut.bind(this) }
-            className='classListButton'>
-            <i className="fas fa-sign-out-alt"></i> 
-            Sign Out
-          </Button>
-          }
-        </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="userInfo">
+				<div className="userName">
+					<Typography component="h1" variant="headline">
+						Welcome {this.props.currentUser}
+					</Typography>
+				</div>
+				<div>
+					{this.props.user === null ? (
+						<Button
+							variant="raised"
+							color="primary"
+							onClick={this.signIn.bind(this)}
+							className="classListButton"
+						>
+							<i className="fas fa-user" />
+							Sign In
+						</Button>
+					) : (
+						<Button
+							variant="raised"
+							color="secondary"
+							onClick={this.signOut.bind(this)}
+							className="classListButton"
+						>
+							<i className="fas fa-sign-out-alt" />
+							Sign Out
+						</Button>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default User;
